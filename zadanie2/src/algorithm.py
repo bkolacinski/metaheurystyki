0d83fe3ed8fca740057c174d7d48482bb683f53b
251554 Kołaciński Bartosz <251554@edu.p.lodz.pl>
2025-10-24 14:57:15 +0200

    Format Algorithm.__init__ signature
    Wrap long parameter list across lines for readability.
    
    Update requirements.txt (binary) to new version.

--- zadanie2/src/algorithm.py
+++ zadanie2/src/algorithm.py
@@ -4,7 +4,8 @@ class Algorithm:
     iter_num: int
     k_coef: float
 
-    def __init__(self, init_temp: float, alpha: float, iter_num: int, k_coef: float):
+    def __init__(self, init_temp: float, alpha: float,
+                 iter_num: int, k_coef: float):
         self.init_temp = init_temp
         self.alpha = alpha
         self.iter_num = iter_num
