e8e370a30b8a93cb7f8856d881241c2dbe786ad2
251554 Kołaciński Bartosz <251554@edu.p.lodz.pl>
2025-10-24 16:51:38 +0200

    Add func parameter and run skeleton
    Add Callable typing and store the objective function on Algorithm.
    Introduce run() method with outer/inner loops and TODOs for neighbor
    selection, acceptance criterion, and temperature update.

--- zadanie2/src/algorithm.py
+++ zadanie2/src/algorithm.py
@@ -1,12 +1,38 @@
+from typing import Callable
+
+
 class Algorithm:
     init_temp: float
     alpha: float
     iter_num: int
     k_coef: float
+    func: Callable[[..., float], float]
 
-    def __init__(self, init_temp: float, alpha: float,
-                 iter_num: int, k_coef: float):
+    def __init__(self, init_temp: float, alpha: float, iter_num: int,
+                 k_coef: float, func: Callable[[..., float], float]):
         self.init_temp = init_temp
         self.alpha = alpha
         self.iter_num = iter_num
         self.k_coef = k_coef
+        self.func = func
+
+    def run(self):
+        stop_condition: bool = False
+        # temp: float = self.init_temp
+        while not stop_condition:
+            for i in range(self.iter_num):
+                pass
+                # TODO ???
+                # wyznaczenie losowo sąsiedniego rozwiązania s’∈
+                # N(s)
+                #
+                # delta: float = func(s_prim) - func(s)
+                # if delta < 0:
+                #     s = s_prim
+                # else:
+                #     x = random.uniform(0, 1)
+                #     if x < math.exp(-delta / (k_coef * temp)):
+                #         s = s_prim
+                #
+                # TODO ???
+                # T = α(T)
