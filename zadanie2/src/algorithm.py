02e382caf0272066cb283e5c01dae6a4522b90b3
bkolacinski <bartoszkolacinski4@gmail.com>
2025-11-04 12:27:54 +0100

    :trollface:

--- zadanie2/src/algorithm.py
+++ zadanie2/src/algorithm.py
@@ -5,34 +5,47 @@ from random import uniform
 
 class SimulatedAnnealing:
     def __init__(self,
-                 func: Callable[[float], float],
-                 domain: tuple[float, float]):
+                 func: Callable,
+                 domain: tuple[float, float] | list[tuple[float, float]]):
         self.func = func
-        self.domain = domain
+        if isinstance(domain, tuple) and len(domain) == 2 and isinstance(
+                domain[0], (int, float)):
+            self.domain = [domain]
+        else:
+            self.domain = domain
+        self.dimensions = len(self.domain)
 
     def run_epochs(self,
                    epochs: int,
                    attempts_per_epoch: int,
                    init_temp: float,
-                   alpha: float) -> tuple[float, float]:
-        left, right = self.domain
-        best_x = uniform(left, right)
-        best_f_x = self.func(best_x)
-        temperature = init_temp
+                   alpha: Callable[[float], float],
+                   k: float) -> tuple[list[float], float]:
+
+        best_point = [uniform(self.domain[i][0], self.domain[i][1])
+                      for i in range(self.dimensions)]
+        best_f_value = self.func(*best_point)
+        temp = init_temp
 
         for _ in range(epochs):
             for _ in range(attempts_per_epoch):
-                x = uniform(best_x - 2 * temperature,
-                            best_x + 2 * temperature)
-                x = max(left, min(x, right))
-                f_x = self.func(x)
+                new_point = []
+                for i in range(self.dimensions):
+                    left, right = self.domain[i]
+                    new_coord = uniform(best_point[i] - 2 * temp,
+                                        best_point[i] + 2 * temp)
+                    new_coord = max(left, min(new_coord, right))
+                    new_point.append(new_coord)
 
-                if f_x > best_f_x:
-                    best_x, best_f_x = x, f_x
+                f_value = self.func(*new_point)
+
+                if f_value > best_f_value:
+                    best_point, best_f_value = new_point, f_value
                 else:
-                    if exp((f_x - best_f_x) / temperature) > uniform(0, 1):
-                        best_x, best_f_x = x, f_x
+                    p = uniform(0, 1)
+                    if exp((f_value - best_f_value) / (k * temp)) > p:
+                        best_point, best_f_value = new_point, f_value
 
-            temperature *= alpha
+            temp = alpha(temp)
 
-        return best_x, best_f_x
+        return best_point, best_f_value
