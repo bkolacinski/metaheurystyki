ffd0ddd00a096e11d75c5a13851a2d80a4672c2e
251554 Kołaciński Bartosz <251554@edu.p.lodz.pl>
2025-11-07 14:19:37 +0100

    feat: enhance GeneticAlgorithm class with population parameter and initial population generation

--- zadanie3/src/genetic_algorithm.py
+++ zadanie3/src/genetic_algorithm.py
@@ -2,10 +2,18 @@ class GeneticAlgorithm:
     def __init__(self,
                  selection_strategy,
                  cross_strategy,
-                 mutation_strategy):
+                 mutation_strategy,
+                 population):
         self.selection_strategy = selection_strategy
         self.cross_strategy = cross_strategy
         self.mutation_strategy = mutation_strategy
+        self.population = population
+
+    def generate_initial_population(self,
+                                    population_size: int):
+        initial_population = []
+
+        return initial_population
 
     def run(self,
             cross_probability: float,
--- zadanie3/src/main.py
+++ zadanie3/src/main.py
@@ -4,14 +4,16 @@ from genetic_algorithm import GeneticAlgorithm
 
 
 def main():
-    data_csv = pd.read_csv('../data/problem plecakowy dane CSV tabulatory.csv', delimiter='\t')
+    data_csv = pd.read_csv('../data/problem plecakowy dane CSV tabulatory.csv',
+                           delimiter='\t', index_col=0).rename_axis(None)
 
-    print(data_csv)
+    print(data_csv.head())
 
     ga = GeneticAlgorithm(
         selection_strategy=None,
         cross_strategy=None,
-        mutation_strategy=None
+        mutation_strategy=None,
+        population=None
     )
 
 
