f732d68739fced203e88d4134ce798e47f23b81f
Nikodem Nowak <71512463+NikodemNowak@users.noreply.github.com>
2025-11-12 21:03:24 +0100

    feat: implement initial population generation and fitness calculation in GeneticAlgorithm class

--- zadanie3/src/genetic_algorithm.py
+++ zadanie3/src/genetic_algorithm.py
@@ -1,3 +1,6 @@
+from random import randint
+
+
 class GeneticAlgorithm:
     def __init__(self,
                  selection_strategy,
@@ -13,11 +16,39 @@ class GeneticAlgorithm:
                                     population_size: int):
         initial_population = []
 
+        for i in range(population_size):
+            individual = [randint(0, 1) for _ in range(26)]
+            initial_population.append(individual)
+
         return initial_population
+
+    def calculate_fitness(self, population) -> list[float]:
+        fitness_values = []
+
+        for individual in population:
+            # TODO: calculate fitness value
+
+        return fitness_values
 
     def run(self,
             cross_probability: float,
             mutation_probability: float,
             population_size: int,
             iterations: int) -> None:
-        pass
+        self.population = self.generate_initial_population(population_size)
+
+        best_solution = None
+        last_solutions = None
+
+        for i in range(iterations):
+            fitness_values = self.calculate_fitness(self.population)
+
+            parents = self.selection_strategy.select(self.population,
+                                                     fitness_values)
+
+            children = self.cross_strategy.cross(parents, cross_probability)
+
+            mutated_children = self.mutation_strategy.mutate(children,
+                                                             mutation_probability)
+
+            self.population = mutated_children
