fe60ca121e6132fd7eb689533b8f1104af9a7f9a
Nikodem Nowak <71512463+NikodemNowak@users.noreply.github.com>
2025-11-16 15:11:08 +0100

    Refactor genetic algorithm for knapsack problem
    Updated GeneticAlgorithm to use item data from a pandas DataFrame, improved initial population generation to respect max weight, and implemented fitness calculation based on item weights and values. Main script now preprocesses CSV data and demonstrates population generation and fitness calculation.

--- zadanie3/src/genetic_algorithm.py
+++ zadanie3/src/genetic_algorithm.py
@@ -1,32 +1,59 @@
-from random import randint
+from random import shuffle
+
+import pandas as pd
 
 
 class GeneticAlgorithm:
+    MAX_WEIGHT = 6404180
+
     def __init__(self,
                  selection_strategy,
                  cross_strategy,
                  mutation_strategy,
-                 population):
+                 population,
+                 items: pd.DataFrame):
         self.selection_strategy = selection_strategy
         self.cross_strategy = cross_strategy
         self.mutation_strategy = mutation_strategy
         self.population = population
+        self.items = items
 
-    def generate_initial_population(self,
-                                    population_size: int):
+    def generate_initial_population(self, population_size: int) -> list[list[int]]:
         initial_population = []
+        num_items = len(self.items)
+        item_indices = list(range(num_items))
+        weights = self.items['Waga'].values
 
-        for i in range(population_size):
-            individual = [randint(0, 1) for _ in range(26)]
+        for _ in range(population_size):
+            individual = [0] * num_items
+            current_weight = 0
+            
+            shuffle(item_indices)
+            
+            for index in item_indices:
+                item_weight = weights[index]
+                if current_weight + item_weight <= self.MAX_WEIGHT:
+                    individual[index] = 1
+                    current_weight += item_weight
+                    
             initial_population.append(individual)
 
         return initial_population
-
-    def calculate_fitness(self, population) -> list[float]:
+    
+    def calculate_fitness(self, population: list[list[int]], items: pd.DataFrame) -> list[float]:
         fitness_values = []
+    
+        weights = items['Waga'].values
+        values = items['Wartosc'].values
 
         for individual in population:
-            # TODO: calculate fitness value
+            total_weight = sum(gene * weight for gene, weight in zip(individual, weights))
+            total_value = sum(gene * value for gene, value in zip(individual, values))
+
+            if total_weight > self.MAX_WEIGHT:
+                fitness_values.append(0.0)
+            else:
+                fitness_values.append(int(total_value))
 
         return fitness_values
 
@@ -35,18 +62,21 @@ class GeneticAlgorithm:
             mutation_probability: float,
             population_size: int,
             iterations: int) -> None:
-        self.population = self.generate_initial_population(population_size)
+        self.population = self.generate_initial_population(
+            population_size)
 
         best_solution = None
         last_solutions = None
 
         for i in range(iterations):
-            fitness_values = self.calculate_fitness(self.population)
+            fitness_values = self.calculate_fitness(
+                self.population, self.items)
 
             parents = self.selection_strategy.select(self.population,
                                                      fitness_values)
 
-            children = self.cross_strategy.cross(parents, cross_probability)
+            children = self.cross_strategy.cross(
+                parents, cross_probability)
 
             mutated_children = self.mutation_strategy.mutate(children,
                                                              mutation_probability)
--- zadanie3/src/main.py
+++ zadanie3/src/main.py
@@ -1,5 +1,4 @@
 import pandas as pd
-
 from genetic_algorithm import GeneticAlgorithm
 
 
@@ -7,14 +6,28 @@ def main():
     data_csv = pd.read_csv('../data/problem plecakowy dane CSV tabulatory.csv',
                            delimiter='\t', index_col=0).rename_axis(None)
 
-    print(data_csv.head())
+    data_csv.columns = [
+        'Nazwa',
+        'Waga',
+        'Wartosc'
+    ]
+
+    data_csv['Waga'] = data_csv['Waga'].astype(str).str.replace(' ', '').astype(int)
+    data_csv['Wartosc'] = data_csv['Wartosc'].astype(str).str.replace(' ', '').astype(int)
+
+    # print(data_csv)
 
     ga = GeneticAlgorithm(
         selection_strategy=None,
         cross_strategy=None,
         mutation_strategy=None,
-        population=None
+        population=None,
+        items=data_csv
     )
+
+    population = ga.generate_initial_population(population_size=100)
+
+    print(ga.calculate_fitness(population, data_csv))
 
 
 if __name__ == "__main__":
